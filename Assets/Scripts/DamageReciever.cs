0552c1d1d7c8cb5e9b060712ef75ed6520ebafc2
Alexander Lovberg <lovbergalexander@gmail.com>
2022-07-07 18:52:36 +0200

    Fixes and updates

--- Assets/Scripts/DamageReciever.cs
+++ Assets/Scripts/DamageReciever.cs
@@ -1,8 +1,8 @@
 using System;
 using EventManager;
+using Unity.Mathematics;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Actor))]
@@ -11,12 +11,13 @@ public class DamageReciever : NetworkBehaviour
     public Actor actor;
     public int maxHealth = 10;
     public NetworkVariable<int> currentHealth;
-    [FormerlySerializedAs("destructable")] public bool autoDestroy = true;
+    public bool autoDestroy = true;
 
     public int lootCount = 1;
     public Loot[] loot;
     private float lootSpawnForce = 5f;
     public bool IsDead => currentHealth.Value == 0;
+    public GameObject destructionEffect;
 
     private void OnValidate()
     {
@@ -38,26 +39,36 @@ public class DamageReciever : NetworkBehaviour
         Events.TriggerEvent(Flag.DamageRecieved, actor, new DamageRecievedArgs()
         {
             reciever = actor,
-            damageReceiver= this,
+            damageReceiver = this,
             damage = 1,
             instigator = bullet.owner,
             destroyed = currentHealth.Value == 0,
         });
 
+        if (destructionEffect)
+        {
+            Instantiate(destructionEffect, bullet.transform.position, Quaternion.LookRotation(-bullet.transform.forward));
+        }
+
         if (IsServer)
         {
             if (autoDestroy && currentHealth.Value == 0)
-            { 
+            {
                 gameObject.GetComponent<NetworkObject>().Despawn();
-                
+
                 for (int i = 0; i < lootCount; i++)
                 {
                     Loot spawnedLoot = Instantiate(loot[Random.Range(0, loot.Length)], transform.position, transform.rotation);
                     Vector3 insideUnitSphere = Random.insideUnitSphere;
                     insideUnitSphere.y = Mathf.Abs(insideUnitSphere.y);
                     spawnedLoot.GetComponent<Rigidbody>().AddForce(insideUnitSphere * lootSpawnForce, ForceMode.VelocityChange);
-                    
+
                     spawnedLoot.GetComponent<NetworkObject>().Spawn();
+                }
+
+                if (destructionEffect != null)
+                {
+                    Instantiate(destructionEffect, transform.position, quaternion.identity);
                 }
             }
         }
